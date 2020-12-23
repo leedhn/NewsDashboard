fdc887cb62955d1790f19cfb2278383c04799a29
b970311 <b970311@yonsei.ac.kr>
2020-12-23 18:46:52 +0900

    feat: add card flip function

--- client/src/component/GridList.js
+++ client/src/component/GridList.js
@@ -4,10 +4,11 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import CardFrame from './card/CardFrame';
+import Front from './card/Front';
+import Back from './card/Back';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const apiURL = "http://localhost:8000";
 
 const GridList = ({category}) => {
   const classes = useStyles();
@@ -16,44 +17,34 @@ const GridList = ({category}) => {
   const [image_url, setImage_url] = useState(null);
   const [title, setTitle] = useState(null);
 
-  const _getLiveData = ({datetime}) => {
-    let items = [];
-    fetch('${apiURL}/test/go/')
-  }
-
-  useEffect(() => {
-    setCategory(category);
-    let items = [];
-    await fetch("test/go")
-      .then(response => response.json())
-      .then((data) => {
-        data.forEach((doc) => {
-          items.push({
-            image_url : doc.image_url,
-            title : doc.title,
-            url : doc.url,
-            keyword : doc.keyword,
-            press : doc.press,
-            title_list
-          })
-        })
-        
-      })
-  })
   return (
     <React.Fragment>
         <CssBaseline/>
           <main>
             <Typography>{currentCategory}</Typography>
             <Container className={classes.cardGrid} maxWidth="md">
-            <Grid container spacing={4}>
-              {cards.map((card) => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
-                  <CardFrame />
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
+              <Grid container spacing={4}>
+                {cards.map((card) => (
+                  <Grid item key={card} xs={12} sm={6} md={4}>
+                    <Flippy className={classes.card}
+                      flipOnHover={false} // default false
+                      flipOnClick={true} // default false
+                      flipDirection="horizontal" // horizontal or vertical
+                      // if you pass isFlipped prop component will be controlled component.
+                      // and other props, which will go to div
+                      // these are optional style, it is not necessary
+                    >
+                      <FrontSide>
+                        <Front />
+                      </FrontSide>
+                      <BackSide>
+                        <Back />
+                      </BackSide>
+                    </Flippy>
+                  </Grid>
+                ))}
+              </Grid>          
+            </Container>
           </main>
     </React.Fragment>
   );  
